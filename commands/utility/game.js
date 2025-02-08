c8d549162ff553aec838931c034debffeb0baaab
Aex Stapp <alexanderstapp@gmail.com>
2025-02-07 22:34:47 -0700

    creates axios request to IGDB

--- commands/utility/game.js
+++ commands/utility/game.js
@@ -1,51 +1,62 @@
 const { SlashCommandBuilder,
-	MessageFlags,
 	ModalBuilder,
 	ActionRowBuilder,
 	TextInputBuilder,
 	TextInputStyle
 } = require('discord.js');
-// const { idgbClientId, idgbAccessToken } = require('./config.json');
-// const apicalypse = require('apicalypse');
 
-// const requestOptions = {
-// 	queryMethod: 'url',
-// 	method: 'post',
-// 	baseURL: 'https://api.igdb.com/v4/',
-// 	headers: {
-// 		'Client-ID': idgbClientId,
-// 		'Authorization': `Bearer ${idgbAccessToken}`
-// 	},
-// 	responseType: 'json',
-// 	timeout: 1000
-// };
+const { idgbClientId, idgbAccessToken } = require('../../config.json');
+const axios = require('axios').default;
 
-const queryModal = new ModalBuilder()
-	.setCustomId('gameQueryModal')
-	.setTitle('Video Game Search');
-
-const queryModalInput = new TextInputBuilder()
-	.setCustomId('gameQueryModalInput')
-	.setLabel('Input video game title')
-	.setStyle(TextInputStyle.Short)
-	.setMaxLength(1_000)
-	.setRequired(true);
-
-const actionRow = new ActionRowBuilder().addComponents(queryModalInput);
-queryModal.addComponents(actionRow);
+const axiosConfig = {
+	method: 'post',
+	maxBodyLength: Infinity,
+	url: 'https://api.igdb.com/v4/search',
+	headers: {
+	  'Client-ID': idgbClientId,
+	  'Content-Type': 'text/plain',
+	  'Authorization': `Bearer ${idgbAccessToken}`
+	},
+	data: ''
+};
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('game')
 		.setDescription('Search for a videogame'),
 	async execute(interaction) {
+		const queryModal = new ModalBuilder()
+			.setCustomId(`gameQueryModal-${interaction.user.id}`)
+			.setTitle('Video Game Search');
+
+		const queryModalInput = new TextInputBuilder()
+			.setCustomId('gameQueryModalInput')
+			.setLabel('Title')
+			.setStyle(TextInputStyle.Short)
+			.setMaxLength(1_000)
+			.setRequired(true);
+
+		const actionRow = new ActionRowBuilder().addComponents(queryModalInput);
+		queryModal.addComponents(actionRow);
+
 		await interaction.showModal(queryModal);
-		// const response = await apicalypse(requestOptions)
-		// 	.query('games')
-		// 	.fields('*')
-		// 	.search();
-		const result = interaction.fields.getTextInputValue('gameQueryModalInput');
-		console.log(result);
-		// await interaction.reply({ content: 'I am done.', flags: MessageFlags.Ephemeral });
+
+		// Wait for modal submission
+
+		const filter = (interaction) => interaction.customId === `gameQueryModal-${interaction.user.id}`;
+
+		interaction
+			.awaitModalSubmit({ filter, time: 30_000 })
+			.then(async (modalInteraction) => {
+				const querySubmission = modalInteraction.fields.getTextInputValue('gameQueryModalInput');
+				axiosConfig.data = `fields *; search "${querySubmission}"; limit 3;`;
+				axios.request(axiosConfig).then(response => {
+					modalInteraction.reply(JSON.stringify(response.data));
+					console.log(JSON.stringify(response.data));
+				});
+			})
+			.catch((err) => {
+				console.log(`Error when getting modal interaction: ${err}`);
+			});
 	}
 };
