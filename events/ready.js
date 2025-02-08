89f1acc6cc76255d54ef459cab7e2dba66394164
Aex Stapp <alexanderstapp@gmail.com>
2025-02-08 00:18:45 -0700

    adds set activity to bot start up

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,11 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
+const getGeneratorResult = require('../services/getGeneratorOutput');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
-		client.user.setActivity('activity test');
+	async execute(client) {
+		client.user.setActivity(await getGeneratorResult('arcanesystems'), { type: ActivityType.Custom });
 		console.log(`Ready. Logged in as self: ${client.user.tag}`);
 	}
 };
